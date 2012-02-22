f5655fbea5c3d50d365e7caa35b297018ad5da0b
François Daoust <fd@tidoust.net>
2012-02-22 15:19:47 +0100

    Code fixed and tested with Google spreadsheet sample.

--- app/res/main.js
+++ app/res/main.js
@@ -9,9 +9,6 @@ var globe;
  * @param {Number} idx The index of the data feed to retrieve and render
  */
 var switchData = function (idx) {  
-  // Set the name of the data feed that will be displayed
-  document.getElementById("currentInfoSpan").innerHTML = Joshfire.factory.config.datasources.main[i].name;
-
   // Fetch the data feed
   Joshfire.factory.getDataSource("main").children[idx].find({}, function (err, data) {
     var formattedData = [],
@@ -26,6 +23,9 @@ var switchData = function (idx) {
       return obj !== obj;
     };
 
+    // Set the name of the data feed that will be displayed
+    document.getElementById("currentInfoSpan").innerHTML = Joshfire.factory.config.datasources.main[idx].name;
+
     if (err || !data || !data.entries) {
       // TODO: alert the user that something went wrong
       return;
@@ -33,73 +33,71 @@ var switchData = function (idx) {
 
     for (i = 0; i < data.entries.length; i++) {
       item = data.entries[i];
-
-      if (item.meta &&
-        item.meta[gsx:lat] &&
-        item.meta[gsx:lng] &&
-        item.meta[gsx:data]) {
-        // Item taken from a Google spreadsheet
-        lat = parseFloat(item.meta[gsx:lat]);
-        lng = parseFloat(item.meta[gsx:lng]);
-        value = parseFloat(item.meta[gsx.data]);
+      
+      // TODO: would be cool to let the user decide which
+      // properties to use to extract the coordinates
+      // and the value to display
+      // Extract coordinates from possible candidates
+      geo = {
+        latitude: item['gsx:lat'] || item['gsx:latitude'],
+        longitude: item['gsx:lng'] || item['gsx:long'] || item['gsx:longitude']
+      };
+      if (geo.latitude && geo.longitude) {
       }
-      else {
-        // TODO: would be cool to let the user decide which
-        // properties to use to extract the coordinates
-        // and the value to display
-        // Extract coordinates from possible candidates
-        if (item.geo) {
-          geo = item.geo;
-        }
-        else if (item.contentLocation) {
-          geo = item.contentLocation.geo;
-        }
-        else if (item.location) {
-          geo = item.location.geo;
-        }
-        else if (item.homeLocation) {
-          geo = item.homeLocation.geo;
-        }
-        else if (item.workLocation) {
-          geo = item.workLocation.geo;
-        }
-        else if (item.jobLocation) {
-          geo = item.jobLocation.geo;
-        }
-        if (!geo || !geo.latitude || !geo.longitude) {
-          continue;
-        }
-        lat = parseFloat(geo.latitude);
-        lng = parseFloat(geo.longitude);
-
-        // Extract value from property candidates
-        value = null;
-        if (item.ratingValue) {
-          value = item.ratingValue;
-        }
-        else if (item.aggregateRating) {
-          value = item.aggregateRating.ratingValue;
-        }
-        else if (item.baseSalary) {
-          value = item.baseSalary;
-        }
-        else if (item.offers && (item.offers.length > 0)) {
-          value = item.offers[0].price;
-        }
-        else if (item.wordCount) {
-          value = item.wordCount;
-        }
-        else if (item.numberOfPages) {
-          value = item.numberOfPages;
-        }
-        else if (geo.elevation) {
-          value = geo.elevation;
-        }
-        if (!value) {
-          continue;
-        }
-        value = parseFloat(value);
+      else if (item.geo) {
+        geo = item.geo;
       }
+      else if (item.contentLocation) {
+        geo = item.contentLocation.geo;
+      }
+      else if (item.location) {
+        geo = item.location.geo;
+      }
+      else if (item.homeLocation) {
+        geo = item.homeLocation.geo;
+      }
+      else if (item.workLocation) {
+        geo = item.workLocation.geo;
+      }
+      else if (item.jobLocation) {
+        geo = item.jobLocation.geo;
+      }
+      if (!geo || !geo.latitude || !geo.longitude) {
+        continue;
+      }
+      lat = parseFloat(geo.latitude);
+      lng = parseFloat(geo.longitude);
+
+      // Extract value from property candidates
+      value = null;
+      if (item['gsx:value'] || item['gsx:data']) {
+        value = item['gsx:value'] || item['gsx:data'];
+      }
+      else if (item.ratingValue) {
+        value = item.ratingValue;
+      }
+      else if (item.aggregateRating) {
+        value = item.aggregateRating.ratingValue;
+      }
+      else if (item.baseSalary) {
+        value = item.baseSalary;
+      }
+      else if (item.offers && (item.offers.length > 0)) {
+        value = item.offers[0].price;
+      }
+      else if (item.wordCount) {
+        value = item.wordCount;
+      }
+      else if (item.numberOfPages) {
+        value = item.numberOfPages;
+      }
+      else if (geo.elevation) {
+        value = geo.elevation;
+      }
+      if (!value) {
+        continue;
+      }
+      value = parseFloat(value);
 
       if (isNaN(lat) || isNaN(lng) || isNaN(value) ||
         (lat < -90) || (lat > 90) ||
@@ -141,6 +139,16 @@ var load = function () {
     i = 0,
     data = null,
     name = null;
+
+  // Update application title and set HTML footer
+  document.getElementById('headtitle').innerHTML = Joshfire.factory.config.app.name;
+  document.getElementById('title').innerHTML = Joshfire.factory.config.app.name;
+
+  if (Joshfire.factory.config.template &&
+    Joshfire.factory.config.template.options &&
+    Joshfire.factory.config.template.options.htmlfooter) {
+    document.getElementById('info').innerHTML = Joshfire.factory.config.template.options.htmlfooter;
+  }
 
   // Check WebGL support
   if (!Detector.webgl) {
